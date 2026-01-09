83303d73bcb6e66211aee59bc6354b95c3288816
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-09 20:15:56 +0530

    event-loop along with thread pool checked

--- event-loop.js
+++ event-loop.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_size = 3;
 
 setTimeout(() => console.log("setTimeout Timer 1"), 0);
 setImmediate(() => console.log("setImmediate Timer 1"), 0);
@@ -10,6 +14,23 @@ fs.readFile("test-file.txt", "utf-8", (err, data) => {
   setTimeout(() => console.log("setTimeout Timer 3"), 3000);
   setImmediate(() => console.log("setImmediate Timer 2"), 0);
   process.nextTick(() => console.log("process.nextTick"));
+
+  // THREAD POOL
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+    // console.log(`${Date.now() - start} password encrypted - ${derivedKey.toString("hex")}`);
+    console.log(`${Date.now() - start} password encrypted 1`);
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+    console.log(`${Date.now() - start} password encrypted 2`);
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+    console.log(`${Date.now() - start} password encrypted 3`);
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+    console.log(`${Date.now() - start} password encrypted 4`);
+  });
+  //   crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  //   console.log(`${Date.now() - start} password encrypted 5`);
 });
 
 console.log("Hello top-level code");
